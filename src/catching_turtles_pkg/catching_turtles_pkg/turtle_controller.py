844a79d4a28c631fe67312f3c52f87b8e43ed1e5
anand <aanandsingh289@gmail.com>
2025-02-17 19:29:07 -0500

    made updates on turtle_controller node

--- src/catching_turtles_pkg/catching_turtles_pkg/turtle_controller.py
+++ src/catching_turtles_pkg/catching_turtles_pkg/turtle_controller.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
+# Subscribe from turtlesim node to get position of alive turtles
+
 import rclpy
 from rclpy.node import Node
+from turtlesim.msg import Pose
 
 class TurtleController(Node):
 
     def __init__(self):
         super().__init__("turtle_controller")
+        self.subscriber_ = self.create_subscription(Pose,"/turtle5/pose",self.callback_position,10)
+        
+    def callback_position(self,msg):
+        print("I am here!!!")
+        print(msg)
         
 def main(args=None):
     rclpy.init(args=args)
